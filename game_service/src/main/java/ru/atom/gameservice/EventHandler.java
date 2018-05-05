538e9219112f1acfbf6689e9dbeda08018ab81c6
nikolay <NLevko@bk.ru>
2018-05-05 21:07:38 +0300

    json

--- game_service/src/main/java/ru/atom/gameservice/EventHandler.java
+++ game_service/src/main/java/ru/atom/gameservice/EventHandler.java
@@ -1,6 +1,5 @@
 package ru.atom.gameservice;
 
-import ru.atom.gameservice.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +9,8 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import ru.atom.gameservice.message.JsonParser;
 import ru.atom.gameservice.network.ConnectionPool;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import org.codehaus.jackson.JsonGenerationException;
 
 @Component
 public class EventHandler extends TextWebSocketHandler implements WebSocketHandler {
@@ -36,7 +33,7 @@ public class EventHandler extends TextWebSocketHandler implements WebSocketHandl
         String gameId = session.getAttributes().get("gameId").toString();
         logger.info("{} connected to game {}", name, gameId);
         connectionPool.addPlayer(session, name);
-//        gameServer.getGameSessionByName(gameId).addPlayer(name);
+        gameServer.getGameSessionByName(gameId).addPlayer(name);
         System.out.println("Socket Connected: " + session);
     }
 
@@ -44,22 +41,7 @@ public class EventHandler extends TextWebSocketHandler implements WebSocketHandl
     protected void handleTextMessage(WebSocketSession session, TextMessage inputMessage) throws Exception {
         String name = connectionPool.getName(session);
         GameSession gameSession = gameServer.getGameSessionByPlayer(name);
-
-        String json = inputMessage.getPayload();
-
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            Message message = mapper.readValue(json, Message.class);
-            message.setName(name);
- //           gameSession.addInput(message);
-            logger.info("JSON -> Message", json);
-            logger.info(message.getData());
-        } catch (JsonGenerationException e) {
-            logger.error("wrong JSON");
-        } catch (IOException e) {
-            logger.error("Can't JSON -> Message");
-            logger.error(e.getLocalizedMessage());
-        }
+        gameSession.addInput(JsonParser.parse(inputMessage.getPayload(), name));
     }
 
     @Override
--- game_service/src/main/java/ru/atom/gameservice/message/Message.java
+++ game_service/src/main/java/ru/atom/gameservice/message/Message.java
@@ -1,29 +1,15 @@
 package ru.atom.gameservice.message;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
 
-//@JsonIgnoreProperties(value = { "name" })
 public class Message {
     private final Topic topic;
-    private final String data;
 
-    //@JsonIgnore
-    private  String name;
+    private  final String name;
 
-    @JsonCreator
-    public Message(@JsonProperty("topic") Topic topic, @JsonProperty("data")String data, @JsonProperty("name")String name) {
+    public Message(Topic topic, String name) {
         this.topic = topic;
-        this.data = data;
         this.name = name;
     }
 
-    //@JsonIgnore
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    //@JsonIgnore
     public String getName() {
         return name;
     }
@@ -32,8 +18,5 @@ public class Message {
         return topic;
     }
 
-    public String getData() {
-        return data;
-    }
 
 }
